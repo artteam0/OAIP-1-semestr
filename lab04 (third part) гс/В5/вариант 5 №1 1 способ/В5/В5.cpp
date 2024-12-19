af35c3f76abe9e99fa430a9269c8ba569fc834f5
artteam0 <154960280+artteam0@users.noreply.github.com>
2024-12-19 18:59:48 +0300

    Add files via upload

---
+++ lab04 (third part) гс/В5/вариант 5 №1 1 способ/В5/В5.cpp
@@ -0,0 +1,30 @@
+﻿#include <iomanip>
+#include <iostream>
+void main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	using namespace std;
+	char c, probel; probel = ' ';
+	cout << "Введите символ "; cin >> c;
+	cout << setw(39) << setfill(probel) << probel;
+	cout << setw(1) << setfill(c) << c << endl;
+	cout << setw(38) << setfill(probel) << probel;
+	cout << setw(3) << setfill(c) << c << endl;
+	cout << setw(37) << setfill(probel) << probel;
+	cout << setw(5) << setfill(c) << c << endl;
+	cout << setw(36) << setfill(probel) << probel;
+	cout << setw(7) << setfill(c) << c << endl;
+	cout << setw(35) << setfill(probel) << probel;
+	cout << setw(9) << setfill(c) << c << endl;
+	cout << setw(34) << setfill(probel) << probel;
+	cout << setw(11) << setfill(c) << c << endl;
+	cout << setw(33) << setfill(probel) << probel;
+	cout << setw(13) << setfill(c) << c << endl;
+	cout << setw(32) << setfill(probel) << probel;
+	cout << setw(15) << setfill(c) << c << endl;
+	cout << setw(31) << setfill(probel) << probel;
+	cout << setw(17) << setfill(c) << c << endl;
+	cout << setw(30) << setfill(probel) << probel;
+	cout << setw(19) << setfill(c) << c << endl;
+
+}
---
+++ lab04 (third part) гс/В5/вариант 5 №2/В5№2/В5№2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iomanip>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_CTYPE, "rus");
+	double cost = 10, startCost = 10, p, n, tempCost;
+	cout << "Введите процент ";
+	cin >> p;
+	cout << "Ведите количество лет ";
+	cin >> n;
+
+	p = p / 100;
+	for (int i = 0; i < n; i++)
+	{
+		tempCost = cost * p;
+		cost += tempCost;
+		tempCost = cost * p;
+		cost -= tempCost;
+
+	}
+	if (startCost > cost) {
+		cout << "Цена уменьшилась на " << startCost - cost << "\n";
+	}
+	else
+	{
+		cout << "Цена увеличилась на " << cost - startCost << "\n";
+	}
+	cout <<"Конечная цена " << cost;
+
+	return 0;
+
+}
