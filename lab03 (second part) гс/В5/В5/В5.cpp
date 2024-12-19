67f7884f3a750939379f085aab7abcda79e47443
artteam0 <154960280+artteam0@users.noreply.github.com>
2024-12-19 18:53:16 +0300

    Add files via upload

---
+++ lab03 (second part) гс/В5/В5/В5.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+void main()
+{
+	double w, v, b = 40, x = 1.1, a = 5e-6;
+	w = (a + b) * tan(x) / (x + 1);
+	v = 1 / 2 * b - sqrt(w - a * b);
+
+	std::cout << "w=" << w <<"\n";
+	std::cout << "v=" << v;
+}
