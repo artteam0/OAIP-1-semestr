fdacef0583cffbbf60bb3956fe2000afba5e7d7e
artteam0 <154960280+artteam0@users.noreply.github.com>
2024-12-19 18:57:53 +0300

    Add files via upload

---
+++ lab04 (second part) гс/В3/вариант 3 №2 1 способ/вариант 3 №2/вариант 3 №2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+int main()
+{
+    setlocale(LC_CTYPE, "rus");
+    int x, y, z;
+    cout << "Введите x: "; cin >> x;
+    cout << "Введите y: "; cin >> y;
+    z = x;
+    x = y;
+    y = z;
+    cout << "x = " << x << endl;
+    cout << "y = " << y;
+}
---
+++ lab04 (second part) гс/В3/вариант 3 №2 2 способ/вариант 3 №2 2 способ/вариант 3 №2 2 способ.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+	setlocale(LC_CTYPE, "rus");
+    int x, y;
+    cout << "Введите x: "; cin >> x;
+    cout << "Введите y: "; cin >> y;
+    x = x + y;
+    y = x - y;
+    x = x - y;
+    cout << "x = " << x << endl;
+    cout << "y = " << y;
+}
