5923588e2c4b9694a13dae73997d28f071c78e62
artteam0 <154960280+artteam0@users.noreply.github.com>
2024-12-19 19:14:12 +0300

    Add files via upload

---
+++ lab09 (fourth part) гс/В6/ЛР 9 В6 парабол/ЛР 9 В6 парабол/ЛР 9 В6 парабол.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	double a = 1, b = 5, n = 200, h, s1 = 0, s2 = 0, i, x, S;
+	h = (b - a) / (2 * n);
+	x = a + 2 * h;
+	for (i = 1; i < n; i++)
+	{
+		s2 += 1 + pow(x, 3);
+		x += h;
+		s1 += 1 + pow(x, 3);
+		x += h;
+	}
+	S = (h / 3) * ((1 + pow(a, 3)) + 4 * (1 + pow(a, 3) + h) + 4 * s1 + 2 * s2 + (1 + pow(b, 3)));
+	cout << "S = " << S << endl;
+	return 0;
+}
---
+++ lab09 (fourth part) гс/В6/ЛР 9 В6 №6/ЛР 9 В6 №6/ЛР 9 В6 №6.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	float a = -100, b = 100, e = 0.0001, x;
+	while (abs(a - b) > (2 * e))
+	{
+		x = (a + b) / 2;
+		if ((5 * x - 1 + pow(x, 3)) * (5 * a - 1 + pow(a, 3)) <= 0)
+		{
+			b = x;
+		}
+		else
+			a = x;
+	}
+	cout << "x = " << x << endl;
+	return 0;
+}
