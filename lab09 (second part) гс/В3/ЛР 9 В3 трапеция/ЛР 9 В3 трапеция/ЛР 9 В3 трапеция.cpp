f4fbe65d325bc13f1f7863a6bcadc20473f1be45
artteam0 <154960280+artteam0@users.noreply.github.com>
2024-12-19 19:12:05 +0300

    Add files via upload

---
+++ lab09 (second part) гс/В3/ЛР 9 В3 трапеция/ЛР 9 В3 трапеция/ЛР 9 В3 трапеция.cpp
@@ -0,0 +1,14 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	double a = 1, b = 6, n = 200, h, s = 0, x;
+	h = (b - a) / n;
+	x = a;
+	for (x; x <= (b - h); x += h)
+	{
+		s += h * ((1 + pow(x, 3)) + (1 + pow(x, 3) + h)) / 2;
+	}
+	cout << "s = " << s << endl;
+	return 0;
+}
