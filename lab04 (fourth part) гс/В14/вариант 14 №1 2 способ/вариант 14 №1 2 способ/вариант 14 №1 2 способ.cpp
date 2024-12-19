c6ccdd1636a67e337a8a43c3f65a6cccd71ad52c
artteam0 <154960280+artteam0@users.noreply.github.com>
2024-12-19 19:00:54 +0300

    Add files via upload

---
+++ lab04 (fourth part) гс/В14/вариант 14 №1 2 способ/вариант 14 №1 2 способ/вариант 14 №1 2 способ.cpp
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+#include <iostream>
+int main()
+{
+	setlocale(LC_CTYPE, "rus");
+	char t;
+	printf("Введите символ: ");
+	scanf_s("%c", &t);
+	printf("         %c\n", t);
+	printf("        %c%c%c\n", t, t, t);
+	printf("       %c%c%c%c%c\n", t, t, t, t, t);
+	printf("      %c%c%c%c%c%c%c\n", t, t, t, t, t, t, t);
+	printf("     %c%c%c%c%c%c%c%c%c\n", t, t, t, t, t, t, t, t, t);
+	printf("    %c%c%c%c%c%c%c%c%c%c%c\n", t, t, t, t, t, t, t, t, t, t, t);
+	printf("   %c%c%c%c%c%c%c%c%c%c%c%c%c\n", t, t, t, t, t, t, t, t, t, t, t, t, t);
+	printf("    %c%c%c%c%c%c%c%c%c%c%c\n", t, t, t, t, t, t, t, t, t, t, t);
+	printf("     %c%c%c%c%c%c%c%c%c\n", t, t, t, t, t, t, t, t, t);
+	printf("      %c%c%c%c%c%c%c\n", t, t, t, t, t, t, t);
+	printf("       %c%c%c%c%c\n", t, t, t, t, t);
+	printf("        %c%c%c\n", t, t, t);
+	printf("         %c\n", t);
+
+}
---
+++ lab04 (fourth part) гс/В14/вариант 14 №2/вариант 14 №2/вариант 14 №2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+	setlocale(LC_CTYPE, "rus");
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	cout << "Введите кол-во сбитых самолётов" << endl;
+	cin >> a;
+	cout << "Введите кол-во сбитых ракет" << endl;
+	cin >> b;
+	cout << "Введите кол-во сбитых спутников" << endl;
+	cin >> c;
+
+	cout << "Всего игрок получил " << 50 * a + 100 * b + 200 * c << " очков";
+}
