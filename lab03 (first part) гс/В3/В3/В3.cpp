5e25f6887e23bf592c5d3aefd1929af0d5b8dd2d
artteam0 <154960280+artteam0@users.noreply.github.com>
2024-12-19 18:51:37 +0300

    Add files via upload

---
+++ lab03 (first part) гс/В3/В3/В3.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+void main()
+{
+	double d, f, i = -6, x = 4.5, z = 1.5e-6;
+	d = tan(-x * i) / sqrt(x - z);
+	f = sin(2 * d) / d;
+	std::cout << "d=" << d;
+	std::cout << "f=" << f;
+
+}
