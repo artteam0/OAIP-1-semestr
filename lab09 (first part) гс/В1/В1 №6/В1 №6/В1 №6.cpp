7464ae658ede4cc7e4c347bb84a1a66bcf2c3df2
artteam0 <154960280+artteam0@users.noreply.github.com>
2024-12-19 19:10:59 +0300

    Add files via upload

---
+++ lab09 (first part) гс/В1/В1 №6/В1 №6/В1 №6.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	float a = -100, b = 100, e = 0.0001, x;
+	while (abs(a - b) > (2 * e))
+	{
+		x = (a + b) / 2;
+		if ((pow (x, 3) + x - 3) * (pow (a, 3) + a - 3) <= 0)
+		{
+			b = x;
+		}
+		else
+			a = x;
+	}
+	cout << "x = " << x << endl;
+	return 0;
+}
