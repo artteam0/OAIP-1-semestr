41f0171c660c22742d8f0548f0999c750cbe363d
artteam0 <154960280+artteam0@users.noreply.github.com>
2024-12-19 19:15:05 +0300

    Add files via upload

---
+++ lab10 (first part) гс/ЛР 10 В5 №1/ЛР 10 В5 №1/ЛР 10 В5 №1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    const int N = 100;
+    int i, sz, C[N];
+    cout << "Введите размер массива: ";
+    cin >> sz;
+    cout << "Массив C: ";
+    srand(time(NULL));
+
+    for ( i = 0; i < sz; i++)
+    {
+        C[i] = rand() % 100;
+        cout << C[i] << " ";
+    }
+
+    for( i = 2; i < sz; i += 3)
+    {
+        C[i] = (C[i - 1] + C[i - 2]) / 2;
+    }
+
+    cout << endl;
+    cout << "Изменённый массив: ";
+
+    for( i = 0; i < sz; i++)
+    {
+        cout << C[i] << " ";
+    }
+
+    return 0;
+}
---
+++ lab10 (first part) гс/ЛР 10 №5 В2/ЛР 10 №5 В2/ЛР 10 №5 В2.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    const int N = 100;
+    int i, sz, f = 0, C[N];
+    cout << "Введите размер массива: ";
+    cin >> sz;
+    cout << "Массив C: ";
+    srand((unsigned)time(NULL));
+
+    for (i = 0; i < sz; i++)
+    {
+        C[i] = rand() % 2;
+        cout << C[i] << " ";
+    }
+    for ( i = 0; i < sz; i++)
+    {
+        if (C[i] == C[sz]) { i += 1; sz -= 1;}
+        else f += 1;
+    }
+    if (f == 0) cout << "Массив симметричен." << endl;
+    else cout << "Массив несимметричен." << endl;
+
+    return 0;
+}
