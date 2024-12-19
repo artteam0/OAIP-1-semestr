dea8943a83368899cab8b902f1ee398f034e7ae1
artteam0 <154960280+artteam0@users.noreply.github.com>
2024-12-19 19:08:29 +0300

    Add files via upload

---
+++ lab08 (first part) гс/ОСН 2 В5/ОСН 2 В5/ОСН 2 В5.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_CTYPE, "rus");
+	double p = -2, y, q = 1;
+	for (int i = 1; i <= 5; i++)
+		
+	{
+		cout << "Введите y: " << endl;
+		cin >> y;
+		q = q * (y - 5);
+	}
+	cout <<"Ответ: " << p + q << endl;
+}
---
+++ lab08 (first part) гс/ОСН В5/ОСН В5/ОСН В5.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_CTYPE, "rus");
+	double a = 0.5, b = 7, v, w, c;
+	for ( int i = 1; i <= 5 ; i++)
+	{
+		cin >> v;
+		if (i == 1)
+		{
+			c = v;
+		}
+		if (v > 0)
+		{
+			w = a + v;
+		}
+		else
+		{
+			w = b / v;
+		}
+		if (w > c)
+		{
+			c = w;
+		}
+		cout << c << endl;
+	}
+	return 0;
+}
