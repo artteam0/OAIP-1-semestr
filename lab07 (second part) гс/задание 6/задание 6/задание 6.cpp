0002f31e27e13122cf580666241644ebcd4e2c68
artteam0 <154960280+artteam0@users.noreply.github.com>
2024-12-19 19:07:49 +0300

    Add files via upload

---
+++ lab07 (second part) гс/задание 6/задание 6/задание 6.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_CTYPE, "rus");
+	double y = 1.4e-3, x = 0.2, m = 4, p, q, j;
+	j = 5;
+
+	while (j <= 11)
+	{
+		cout << j << endl;
+		p = exp(sin(j / x)) * log(x / y) * x;
+		cout << p << endl;
+
+		if (p <= (y * y))
+		{
+			q = sqrt(p / m);
+		}
+
+		else
+
+			if (p > (y * y))
+			{
+				q = (sqrt(2 * x)) / (j + p);
+			}
+
+		cout << q << endl;
+		
+		j = j + 2;
+	}
+	return 0;
+}
---
+++ lab07 (second part) гс/задание 7/задание 7/задание 7.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_CTYPE, "rus");
+    int N, num, temp, count;
+    cout << "Введите количество чисел: ";
+    cin >> N;
+
+    num = 1;
+    count = 0;
+
+    while (count < N) {
+        int temp = num;
+
+        while (temp % 2 == 0) {
+            temp /= 2;
+        }
+
+        while (temp % 3 == 0) {
+            temp /= 3;
+        }
+
+        while (temp % 5 == 0) {
+            temp /= 5;
+        }
+
+        if (temp == 1&num>1) {
+            cout << num << " ";
+            count++;
+        }
+
+        num++;
+    }
+
+    return 0;
+}
