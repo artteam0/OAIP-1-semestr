8fa5cc06c07c9cce84f1c224cb4f49eb41f3bd04
artteam0 <154960280+artteam0@users.noreply.github.com>
2024-12-21 11:44:33 +0300

    Add files via upload

---
+++ lab12 (first part) гс/В5/лр13 В5 №1/лр13 В5 №1/лр13 В5 №1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+	char a[100];
+	int i, j, e = 0;
+	gets_s(a, 99);
+	while (a[e] != '\0')
+	{
+		e += 1;
+	}
+	for ( i = 0; i < e - 1; i++)
+	{
+		if (a[i] == '(')
+		{
+			i = i - 1;
+			while (a[i] != ')') 
+			{
+				a[i] = '\a';
+				i += 1;
+			}
+			a[i] = '\a';
+		}
+	}
+	cout << a;
+	return 0;
+}
---
+++ lab12 (first part) гс/В5/лр13 В5 №2/лр13 В5 №2/лр13 В5 №2.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main() 
+{
+	char a[100], b[100], c[100];
+	int i, e = 0,k=0;
+	gets_s(a, 99);
+	while (a[e] != '\0')
+	{
+		e += 1;
+	}
+
+	for (i = 0; i < e-1; i++)
+	{
+		if (a[i] == ' ' || a[i] == a[0])
+		{
+			i += 1;
+			if (a[i - 1] == a[0]) i -= 1;
+			while (a[i] != ' ' || a[i]!='\0')
+			{
+				b[k] = a[i];
+				k += 1;
+				i += 1;
+			}
+			b[k] = ' ';
+			k += 1;
+			i -= 1;
+		}
+	}
+
+
+	for (i = 0; i < k; i++)
+	{
+		cout << b[i];
+	}
+
+
+	/*for ( i = 0; i < e; i++)
+	{
+		if (a[i] > b[i])
+		{
+			cout << a;
+			break;
+		}
+		else
+		{
+			if (b[i] > a[i])
+				cout << b;
+		}
+	}*/
+}
