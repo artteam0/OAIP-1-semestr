8251489f986522cd2d77f3a5de31d7ec4beaf8fd
artteam0 <154960280+artteam0@users.noreply.github.com>
2024-12-21 12:00:41 +0300

    Add files via upload

---
+++ lab14 (fourth part) гс/доп/лр14 доп1/лр14 доп1/лр14 доп1.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream> 
+#include <time.h> 
+#include <stdlib.h> 
+using namespace std;
+
+void main()
+{
+	int A[100][100], i, j, znak, n;
+	srand(time(NULL));
+	cout << "n:";
+	cin >> n;
+	cout << endl << endl << endl;
+	for (i = 0; i < 2 * n; i++)
+	{
+		for (j = 0; j < 2 * n; j++)
+		{
+			A[i][j] = rand() % 11;
+			znak = rand() % 2;
+			if (znak == 1)A[i][j] = A[i][j] * -1;
+			cout << A[i][j] << "      ";
+		}
+		cout << endl << endl << endl;
+	}
+	cout << endl << endl << endl;
+	cout << endl << endl << endl;
+	cout << endl << endl << endl;
+	cout << endl << endl << endl;
+	swap(A[0][0], A[n - 1][n - 1]);
+	swap(A[0][n - 1], A[n - 1][0]);
+	swap(A[0][n], A[n - 1][2 * n - 1]);
+	swap(A[0][2 * n - 1], A[n - 1][n]);
+
+	swap(A[n][0], A[2 * n - 1][n - 1]);
+	swap(A[n][n - 1], A[2 * n - 1][0]);
+	swap(A[n][n], A[2 * n - 1][2 * n - 1]);
+	swap(A[n][2 * n - 1], A[2 * n - 1][n]);
+
+	for (i = 0; i < 2 * n; i++)
+	{
+		for (j = 0; j < 2 * n; j++)
+		{
+			cout << A[i][j] << "      ";
+		}
+		cout << endl << endl << endl;
+	}
+}
---
+++ lab14 (fourth part) гс/доп/лр14 доп3/лр14 доп3/лр14 доп3.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream> 
+using namespace std;
+
+void main()
+{
+    float A[100][100], B[100];
+    int i, j, n, max = -999, cntrl1 = 0, cntrl2 = 0, count = 1, diogonal = 1, k = 1, help;
+    cout << "n:";
+    cin >> n;
+    cout << endl;
+
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= n; j++)
+        {
+            cin >> A[i][j];
+        }
+    }
+
+    for (help = 1; help <= n; help++)
+    {
+        for (i = 1; i <= n; i++)
+        {
+            for (j = 1; j <= n; j++)
+            {
+                if (A[i][j] > max)
+                {
+                    max = A[i][j];
+                    cntrl1 = i;
+                    cntrl2 = j;
+                }
+            }
+        }
+        B[count] = max;
+        A[cntrl1][cntrl2] = 0;
+        count += 1;
+        max = -999;
+    }
+
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= n; j++)
+        {
+            if (A[i][j] == 0)
+            {
+                A[i][j] = B[k]; k += 1;
+                swap(A[i][j], A[diogonal][diogonal]);
+                diogonal += 1;
+            }
+        }
+    }
+
+
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= n; j++)
+        {
+            cout << A[i][j] << "      ";
+
+        }
+        cout << endl << endl << endl << endl;
+    }
+}
